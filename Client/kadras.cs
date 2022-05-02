d11289d4b5f54d941fb5befdbddac48df68c04f1
Bartosz Gąciarek <bartekgaciarek1@gmail.com>
2022-05-02 22:50:24 +0200

    V2

---
+++ Client/kadras.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class kadras
+    {
+        public int numer { get; set; }
+        public string imie_nazwisko { get; set; }
+        public int wiek { get; set; }
+
+        public string narodowosc { get; set; }
+        public string pozycja { get; set; }
+
+    }
+}
