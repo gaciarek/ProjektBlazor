c05bd2e4519648e93e029afd4ee6d1d349ccfbec
Bartosz Gąciarek <bartekgaciarek1@gmail.com>
2022-05-03 14:40:25 +0200

    v3

---
+++ Client/bramki.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class bramki
+    {
+        public int id { get; set; }
+        public string imie_nazwisko { get; set; }
+        public int PL { get; set; }
+
+        public int LM { get; set; }
+        public int FA { get; set; }
+        public int LA { get; set; }
+        public int suma { get; set; }
+
+
+    }
+}
---
+++ Client/ligi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class ligi
+    {
+        public int miejsce { get; set; }
+        public string druzyna { get; set; }
+        public int mecze { get; set; }
+        public int zwyciestwo { get; set; }
+        public int remis { get; set; }
+        public int porazka { get; set; }
+        public int gz { get; set; }
+
+        public int gs { get; set; }
+        public int bilans { get; set; }
+
+        public int punkty { get; set; }
+
+    }
+}
